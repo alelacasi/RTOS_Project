c203419ea55d94bd77e20a59b8f46525b0d848cd
ale <ale-laca@hotmail.com>
2020-12-03 18:27:16 -0500

    Bug fixed

--- main.cpp
+++ main.cpp
@@ -12,12 +12,12 @@ using namespace std;
 const int NUMOFTRAITS = 55;
 
 // Real Filepath /public/coen320/Driving Data(KIA SOUL)_(150728-160714)_(10 Drivers_A-J)
-map<string, int> read_csv(string filename){
-    // Reads a CSV file into a vector of <string, vector<int>> pairs where
+map<string, string> read_csv(string filename){
+    // Reads a CSV file into a vector of <string, vector<string>> pairs where
     // each pair represents <column name, column values>
 
     // Create a vector of <string, int vector> pairs to store the result
-    map<string, int> result;
+    map<string, string> result;
 
     // Create an input filestream
     ifstream myFile(filename);
@@ -39,62 +39,47 @@ map<string, int> read_csv(string filename){
 
         // Create a stringstream from line
         stringstream ss(line);
-		int i=0;
+		int count=0;
 
         // Extract each column name
         while(getline(ss, colname, ',')){
-            names[i] = (colname);
-			i++;
+            names[count] = (colname);
+			//cout<<names[count]<<endl;
+			count++;
         }
+		//cout<<endl;
 		getline(myFile, line2);
 		stringstream new_ss(line2);
-		i=0;
+		count=0;
 
 		while(getline(new_ss, colname, ',')){
-            val[i] = (colname);
-			i++;
+            val[count] = (colname);
+			//cout<<val[count]<<endl;
+			count++;
         }
-
-		/*while(strsr >> hold){
-			cout<<hold<<endl;
-			val[i] = hold;
-			result.insert(pair<string, int>(names[i], val[i]));
-            
-			//if(ss.peek() == ',') ss.ignore();
-            
-            // Increment the column index
-            i++;
-        }*/
 
 		// Add the current column's values map
 		for(int i = 0; i<NUMOFTRAITS; i++){
-			try{
-				result.insert(pair<string, int>(names[i], stoi(val[i])));
-			}
-			catch(invalid_argument){
-				cout<<"Error"<<endl;
-			}
-			
+			cout<<names[i]<<"\t"<< val[i]<<endl;
+			result.insert(pair<string, string>(names[i], val[i]));
 		}
 	}
-
     // Close file
     myFile.close();
+	
+	cout << "\nThe map is : \n"; 
+	cout << "\tKEY\tELEMENT\n"; 
+	for(int i = 0; i<NUMOFTRAITS; i++){
+		cout<<names[i]<<"\t"<< result[names[i]]<<endl;
+	}
+	cout << endl; 
 
     return result;
 }
 int main (int argc, char *argv[]) {
-	map<string,int> dictionary;
+	map<string,string> dictionary;
 	for(int i =0; i<1; i++){
-		dictionary = read_csv("Driving Data(KIA SOUL)_(150728-160714)_(10 Drivers_A-J).csv");
-		map<string, int>::iterator itr; 
-		cout << "\nThe map is : \n"; 
-		cout << "\tKEY\tELEMENT\n"; 
-		for (itr = dictionary.begin(); itr != dictionary.end(); ++itr) { 
-			cout << '\t' << itr->first 
-				<< '\t' << itr->second << '\n'; 
-		} 
-		cout << endl; 
+		dictionary = read_csv("Driving Data(KIA SOUL)_(150728-160714)_(10 Drivers_A-J).csv"); 
 	}
 
 	return 0;
